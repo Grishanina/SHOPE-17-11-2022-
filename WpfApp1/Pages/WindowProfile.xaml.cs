dda85b37b0b2bc35a040ffca27063b6671114fd3
ГришанинаКИ <@106-04>
2022-11-17 18:20:03 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Pages/WindowProfile.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Логика взаимодействия для WindowProfile.xaml
+	/// </summary>
+	public partial class WindowProfile : Window
+	{
+		T_Users user;
+		public WindowProfile(T_Users user)
+		{
+			InitializeComponent();
+			this.user = user;
+			tbName.Text = user.Name;
+			tbSurname.Text = user.Surname;
+			tbPatronymic.Text = user.Patronymic;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			user.Name = tbName.Text;  
+			user.Surname = tbSurname.Text;  
+			tbPatronymic.Text = user.Patronymic;
+			ClassBase.BD.SaveChanges();  
+			this.Close();  
+		}
+	}
+}
